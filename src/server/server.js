9f221863091008119f31c3e5ca0ba8144e1a5a52
joshSteadman <josh.steadman15@gmail.com>
2021-02-19 11:05:35 +0000

    initial commit

---
+++ src/server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 8000;
+const table ='users';
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+    next();
+  });
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PWD,
+  database: process.env.MYSQL_DB,
+});
+
+
+
+app.listen(port, () => {
+  console.log(`App server now listening to port ${port}`);
+});
+
+app.get('/api/users', (req, res) => {
+  pool.query(`select * from ${table}`, (err, rows) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(rows);
+    }
+  });
+});
